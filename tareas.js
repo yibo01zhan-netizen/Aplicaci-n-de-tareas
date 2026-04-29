4a55753b8e911c9eef5ddde4e0e8e45e6b1aee6a
unknown <admin@BGAI202-008.bga.upb>
2026-04-28 21:05:07 -0500

    Cree main

---
+++ tareas.js
@@ -0,0 +1,18 @@
+console.log(" --Aplicacion de tareas-- ")
+
+
+const titulo = prompt("Ingrese el titulo de la tarea")
+const descripcion = prompt("Ingerese la descripcion de la tarea")
+const complet = prompt("Ingrese estado de la tarea")
+
+function crearTarea(){
+if (tareaEstado == "disponible"){
+  console.log(`${titulo}: ${item}`)
+}
+else {
+  console.log("Tarea no disponible")
+}
+}
+
+const filter = prompt("Ingrese tarea filtrar")
+const tareafiltro = tarea.filter(item )
